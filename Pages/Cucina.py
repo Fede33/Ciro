8658c28e825bc5918655f1392ea1a2f0ad029722
PietroColaguori <pietro.colaguori3889@gmail.com>
2023-03-28 11:22:43 +0200

    Update Cucina.py (UI, Database)
    Si connette al database, gestisce le collezioni 'ordini_cameriere' ed 'ordini_cameriere_completati', gli ordini sono mostrati tramite st.expander, il conteggio funziona. Per provarlo leggere i commenti ed usarli per popolare ed eliminare le collezioni.

--- Pages/Cucina.py
+++ Pages/Cucina.py
@@ -8,8 +8,8 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # TO-DO:
-# Gestire eliminazione ordine completato da "ordini_cameriere"
-# Gestire spostamente ordine completato in "ordini_cameriere_completati"
+# Gestire eliminazione dei record alle 00.00 di ogni giorno sia in 'ordini_cameriere' che 'ordini_cameriere_completati'
+# Gestire il refresh della pagina periodico per non "perdersi" nuovi ordini provenienti da Sala.py
 
 # Accedo al database di Ciro
 if not firebase_admin._apps:
@@ -54,10 +54,14 @@ ordine3 = {
     "tavolo":3 # fornito dalla sala
 }
 
+""" # --DEBUG - riempio la collezione 'ordini_cameriere'
 if(len(db.collection("ordini_cameriere").get()) != 4): # solo documento dummy -> runnato solo la prima volta
     db.collection("ordini_cameriere").document("ordine1").set(ordine1)
     db.collection("ordini_cameriere").document("ordine2").set(ordine2)
     db.collection("ordini_cameriere").document("ordine3").set(ordine3)
+"""
+st.markdown("##")
+st.markdown("""---""")
 
 # Contatore ordini completati e numero ordini totali
 ordini_completati = 0
@@ -66,33 +70,61 @@ if "ordini_completati" not in st.session_state:
 
 ordini_totali = len(db.collection("ordini_cameriere").get())
 
-# Funzione per mostrare gli ordini
-def display_orders():
-    st.header("Lista degli ordini")
-    docs = db.collection("ordini_cameriere").stream()
-    for doc in docs:
-        doc_data = doc.to_dict()
-        print(doc_data)
-        if("dummy" not in doc_data.keys()):
-            header = doc_data['timestamp'] + "\n" + doc_data["nome"] + "\n" + "Tavolo: " + str(doc_data["tavolo"])
-            with st.expander(header):
-                button = st.button("Completato! [" + str(doc_data["id"]) + "]")
-                if button:
-                    st.success("Ordine completato!")
-                    # ordine eliminato dal database e spostato in apposita collezione per ordini completati  --TO-DO
-                    st.session_state.ordini_completati += 1
-    
-display_orders()
+# Costruisco la UI
+st.header("Lista degli ordini")
+st.subheader("Clicca sulla notifica per segnare come completato")
+docs = db.collection("ordini_cameriere").stream()
+for doc in docs:
+    doc_data = doc.to_dict()
+    print(doc_data)
 
-# Mostra numero di ordini completati su totali
-st.write(f"## Numero di Ordini Completati: {st.session_state.ordini_completati}/{ordini_totali - 1}")
+    # Il dummy viene ignorato e non può essere manipolato
+    if("dummy" not in doc_data.keys()):
+        header = doc_data["nome"] + " - Tavolo: " + str(doc_data["tavolo"]) + " [" + doc_data['timestamp'] + "]"
+        with st.expander(header):
+            button = st.button("Completato! [" + str(doc_data["id"]) + "]")
+            if button:
+                st.success("Ordine completato!")
+                
+                # Ordine eliminato dal database
+                docName = "ordine" + str(int(doc_data["id"]) + 1)
+                print("=== " + docName + " ===")
+                db.collection("ordini_cameriere").document(docName).delete()
 
-""" --DEBUG
+                # Aggiungo elemento agli ordini completati
+                completati_ref = db.collection("ordini_cameriere_completati")
+                nome_doc = "ordine" + str(st.session_state.ordini_completati)
+                doc_data['ora_completato'] = time.strftime("%H:%M:%S")
+                completati_ref.document(nome_doc).set(doc_data)
+
+                # Aggiorno UI
+                st.session_state.ordini_completati += 1
+                docs = db.collection("ordini_cameriere").stream()
+                st.experimental_rerun()
+
+
+# Se un solo elemento in 'ordini_completati' => solo il dummy
+if(ordini_totali == 1):
+    st.session_state.ordini_completati = 0
+    st.success("Tutti gli ordini sono stati completati!")
+else:
+    st.write(f"## Numero di Ordini Completati: {st.session_state.ordini_completati}/{ordini_totali - 1 + st.session_state.ordini_completati}")
+
+st.markdown("""---""")
+st.markdown("##")
+st.markdown("##")
+st.markdown("##")
+
+#--DEBUG Scommentare per ricondursi al caso in cui non ci sono ancora ordini completati
+#--DEBUG Scommentando sia questo che il codice precedente si ci riconduce al caso base con 3 ordini
+#--DEBUG Dopo run con commenti scommentati, ricommentarli
+"""
 docs = db.collection("ordini_cameriere_completati").stream()
 for doc in docs:
     doc.reference.delete()
 db.collection("ordini_cameriere_completati").document().delete()
-
+"""
+# --DEBUG Display di tutti gli elementi nel database, poco carino ma funziona
 for collection_ref in db.collections():
     st.write(f"Collection: {collection_ref.id}")
     # Get all documents in the collection
@@ -102,5 +134,5 @@ for collection_ref in db.collections():
         st.write(f"Document: {doc.id}")
         doc_data = doc.to_dict()
         for key, value in doc_data.items():
-            st.write(f"{key}: {value}")  """
+            st.write(f"{key}: {value}")
     
